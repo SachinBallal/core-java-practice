157e243816c4153f44a472841a0c91072f9d1c54
SachinBallal <117001781+SachinBallal@users.noreply.github.com>
2026-02-14 13:53:32 +0530

    Create JavaBasicsPractice.java

---
+++ JavaBasicsPractice.java
@@ -0,0 +1,68 @@
+package javaforQA;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MyClass extends MyClassP {
+	
+		
+	 public static void main(String[] args) {
+		// TODO Auto-generated method s
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=i; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				if(i==j) {
+					System.out.print("$");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				if(i==j) {
+					System.out.print("$");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+			
+	}}
